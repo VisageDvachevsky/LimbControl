66bc5653ce55bb76085f91c0f931d179f5e01ed8
Visage <82640121+AaaaaaaaAaaaaaaaaaaaaaaaaa4217@users.noreply.github.com>
2023-03-20 18:50:11 +0300

    Update Main.py

--- Python/Main.py
+++ Python/Main.py
@@ -14,6 +14,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         ret, frame = cap.read()
 
         ret_v, frame_v = video.read()
+        
         if not ret_v:
             break
 
@@ -37,24 +38,28 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             for i, landmark in enumerate(results.pose_landmarks.landmark):
                 CameraArr.append([landmark.x, landmark.y, landmark.z])
 
-        elif results_v.pose_landmarks is not None:
+        if results_v.pose_landmarks is not None:
             for i, landmark in enumerate(results_v.pose_landmarks.landmark):
                 VideoArr.append([landmark.x, landmark.y, landmark.z])
 
-        elif len(CameraArr) != len(VideoArr):
+        if len(CameraArr) != len(VideoArr):
             min_length = min(len(CameraArr), len(VideoArr))
             CameraArr = CameraArr[:min_length]
             VideoArr = VideoArr[:min_length]
 
         distance = 0
+        percent = 0
         for i in range(len(CameraArr)):
             x1, y1, z1 = CameraArr[i]
             x2, y2, z2 = VideoArr[i]
             distance += math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
+            percent = (1 - (distance / 100)) * 100
+
 
         flipped_image = cv2.flip(image, 1)
         flipped_image = cv2.cvtColor(flipped_image, cv2.COLOR_RGB2BGR)
-        cv2.putText(flipped_image, f"Distance: {distance:.2f}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        print(distance)
+        cv2.putText(flipped_image, f"Accuracy in percent: {percent:.1f}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         cv2.imshow('MediaPipe Pose', flipped_image)        
         if cv2.waitKey(1) & 0xFF == ord('q'):
